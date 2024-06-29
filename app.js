b31934efed7b2fff7d481aab1d6bd06550a11c18
Shubham Swarnkar <107606605+shubhamswarnkar0611@users.noreply.github.com>
2024-06-29 20:08:38 +0530

    added product and user model

---
+++ app.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById('5baa2528563f16379fc8a610')
+    .then(user => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(
+    'mongodb+srv://2tlmlhtr:Shubham123@cluster0.4zyglzr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  )
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Shubham',
+          email: 'shubham@gmail.com',
+          cart: {
+            items: []
+          }
+        })
+        user.save();
+        app.listen(3000);
+      }
+    })
+   
+    console.log('Connected to MongoDB');
+  })
+  .catch(err => {
+    console.log(err);
+  });
